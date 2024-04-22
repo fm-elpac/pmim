5367bdaec8959525df084d212881f90ad58d9e91
secext2022 <secext2022@outlook.com>
2024-04-23 05:33:07 +0800

    更新(依赖): deno 1.42.4, 移除 tailwindcss

--- server/fresh.config.ts
+++ server/fresh.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "$fresh/server.ts";
-import tailwind from "$fresh/plugins/tailwind.ts";
 
 import { 监听地址 } from "./pmims/conf.ts";
 import { onListen, 获取端口 } from "./pmims/mod.ts";
 
 export default function getConfig() {
   return defineConfig({
-    plugins: [tailwind()],
+    plugins: [],
 
     server: {
       port: 获取端口(),
--- server/pmims/conf.ts
+++ server/pmims/conf.ts
@@ -1,5 +1,5 @@
 // 程序版本号
-export const P_VERSION = "pmim-server v0.1.4";
+export const P_VERSION = "pmim-server v0.1.5";
 // 日志前缀
 export const P_LOG = "pmims";
 
